2aa4028b5cab7acfeba38d399d07bd07542668d6
devrajn <devraj.nepal@gmail.com>
2023-02-01 13:28:01 -0600

    read me updated

---
+++ README.md
@@ -0,0 +1,47 @@
+#My Blog App 
+
+This blog app is my second project.</br>
+
+Inspired to write this project as I didnt find the any blog posts that covers varieties of topics. Yet there are many bloggers and blog pages but thery are not visually attractive.
+</hr>
+
+<h4>TechnologiesIncluded </h4>
+<ol>
+<li>HTML</li>
+<li>CSS</li>
+<li>Javascript</li>
+<li>Bootstrap</li>
+<li>Liquidjs</li>
+<li>Nodejs</li>
+<li>Express</li>
+<li>MongoDB</li>
+<li>Mongoose</li>
+</ol>
+</hr>
+<h4>As a user I want the ability to </h4>
+
+<ul>
+<li>SigninL</li>
+<li>SignUP</li>
+<li>Create Blog Posts</li>
+<li>Comment</li>
+<li>Delete Comments</li>
+<li>Edit Posts</li>
+</ul>
+</hr>
+
+![Alt text](images/sign_inhot%202023-02-01%20at%201.08.54%20PM.png)
+
+![Alt text](images/CreatePost.png)
+
+![Alt text](images/comments.png)
+
+![Alt text](images/mobilenavigation.png)
+
+![Alt text](images/Screenshot%202023-02-01%20at%208.35.21%20AM.png)
+
+</hr>
+
+<p>ERD</p>
+
+
--- models/seed.js
+++ models/seed.js
@@ -15,24 +15,83 @@ const db = mongoose.connection
 db.on('open', () => {
     // array of starter resources(blogs)
     const seedBlog = [
-        {
-          title: 'nodejs',
-          content: 'This is the content of the first blog post.',
-          author: 'wiki'
+         
+      {
+        title: "Meditation",
+        content: "Meditation has been shown to have numerous benefits for mental and physical health. It can reduce stress, increase focus and creativity, and improve overall well-being. Whether you're a beginner or an experienced meditator, incorporating this practice into your daily routine can bring about positive changes in your life.",
+        author: "Sarah Smith"
         },
-        {title:What Does Lorem Ipsum Mean?
-          Lorem ipsum, in graphical and textual context, refers to filler text that 
-          is placed in a document or 
-          visual presentation. Lorem ipsum is derived from 
-          the Latin "dolorem ipsum" roughly translated as "pain itself."
+        {
+        title: "The Importance of Sleep",
+        content: "Getting enough sleep is crucial for our physical and mental health. It helps to boost our immune system, improve memory, and increase our overall mood. However, many people struggle to get enough quality sleep. In this post, we'll explore some tips and tricks for getting a better night's rest.",
+        author: "John Doe"
+        },
+        {
+        title: "The Power of Positive Thinking",
+        content: "Positive thinking has been shown to have a powerful impact on our mental and emotional well-being. It can help us to overcome challenges, boost our self-esteem, and improve our relationships with others. In this post, we'll discuss the benefits of positive thinking and how you can incorporate it into your daily life.",
+        author: "Jane Doe"
+        },
+        {
+        title: "Exercise",
+        content: "Exercise is not only good for our physical health, but it also has numerous benefits for our mental well-being. Regular exercise can reduce stress, improve mood, and boost self-esteem. Whether you're a beginner or an experienced athlete, incorporating exercise into your daily routine can bring about positive changes in your life.",
+        author: "Mike Johnson"
+        },
+        {
+        title: "The Benefits of Mindfulness",
+        content: "Mindfulness has become a popular trend in recent years due to its numerous benefits. It can help to reduce stress, improve focus, and increase overall well-being. Whether you're a beginner or an experienced practitioner, incorporating mindfulness into your daily routine can bring about positive changes in your life.",
+        author: "Laura Williams"
+        },
+        
+          {
+          title: "The Importance of Hydration",
+          content: "Staying hydrated is crucial for our physical and mental health. It helps to boost our energy levels, improve digestion, and keep our skin looking youthful. In this post, we'll discuss the benefits of hydration and how you can make sure you're getting enough water every day.",
+          author: "David Brown"
+          },
+          {
+          title: "The Power of Gratitude",
+          content: "Gratitude has been shown to have a powerful impact on our mental and emotional well-being. It can help us to overcome challenges, boost our self-esteem, and improve our relationships with others. In this post, we'll discuss the benefits of gratitude and how you can incorporate it into your daily life.",
+          author: "Sarah Johnson"
+          },
+          {
+          title: "The Benefits of a Healthy Diet",
+          content: "Eating a healthy diet is crucial for our physical and mental health. It helps to boost our energy levels, improve digestion, and reduce the risk of chronic diseases. In this post, we'll discuss the benefits of a healthy diet and how you can make sure you're getting all the nutrients you need every day.",
+          author: "John Smith"
+          },
           
-          Lorem ipsum presents the sample font and orientatin
-          of writing on web pages and other software applications where 
-          content is not the main concern of the developer.
-           
-          author:google
-        },
-        {
+          {
+            title: "The Importance of Hydration",
+            content: "Staying hydrated is crucial for our physical and mental health. It helps to boost our energy levels, improve digestion, and keep our skin looking youthful. In this post, we'll discuss the benefits of hydration and how you can make sure you're getting enough water every day.",
+            author: "David Brown"
+          },
+          
+          {
+            title: "The Power of Gratitude",
+            content: "Gratitude has been shown to have a powerful impact on our mental and emotional well-being. It can help us to overcome challenges, boost our self-esteem, and improve our relationships with others. In this post, we'll discuss the benefits of gratitude and how you can incorporate it into your daily life.",
+            author: "Sarah Johnson"
+          },
+          
+          {
+            title: "The Benefits of a Healthy Diet",
+            content: "Eating a healthy diet is crucial for our physical and mental health. It helps to boost our energy levels, improve digestion, and reduce the risk of chronic diseases. In this post, we'll discuss the benefits of a healthy diet and how you can make sure you're getting all the nutrients you need every day.",
+            author: "John Smith"
+          },
+          {
+            title: "The Importance of Self-Care",
+            content: "Self-care is important for our physical and mental well-being. It helps to reduce stress, improve mood, and boost self-esteem. Whether you're a busy professional or a stay-at-home parent, incorporating self-care into your daily routine can bring about positive changes in your life.",
+            author: "Jane Johnson"
+          },
+          
+          {
+            title: "nodejs",
+            content: "This is the content of the first blog post.",
+            author: "wiki"
+          },
+          // {
+          //   title: "lorem_ipsum",
+          //   content: "Lorem ipsum presents the sample font and orientation of writing on web pages and other software applications where content is not the main concern of the"
+          //   author: "google"
+          // }
+                    {
           title: 'bp2',
           content: 'This is the content of the second blog post.',
           author: 'bw2'
@@ -43,7 +102,7 @@ db.on('open', () => {
           author: 'bw3 '
         },
         {
-          title: 'First Blog Post',
+          title: "First Blog Post",
           content: 'This is the content of the first blog post.',
           author: 'bw4'
         },
@@ -62,62 +121,19 @@ db.on('open', () => {
           content: 'This is the content of the first blog post.',
           author: 'bw6'
         },
+        // {
+        //   title: "ML",
+        //   content:"Introduction to Machine Learning: Machine learning is a type of 
+        //   artificial intelligence that allows software applications
+        //    to become more accurate in predicting outcomes without being 
+        //    explicitly programmed.",
+        //    author:"search"
+        // },
         {
           title: 'bp9',
           content: 'This is the content of the second blog post.',
           author: 'bw9'
-        },
-        // {
-        //   title: 'Third Blog Post',
-        //   content: 'This is the content of the third blog post.',
-        //   author: 'Bob Johnson'
-        // },
-        // {
-        //   title: 'First Blog Post',
-        //   content: 'This is the content of the first blog post.',
-        //   author: 'John Doe'
-        // },
-        // {
-        //   title: 'Second Blog Post',
-        //   content: 'This is the content of the second blog post.',
-        //   author: 'Jane Smith'
-        // },
-        // {
-        //   title: 'Third Blog Post',
-        //   content: 'This is the content of the third blog post.',
-        //   author: 'Bob Johnson'
-        // },
-        // {
-        //   title: 'First Blog Post',
-        //   content: 'This is the content of the first blog post.',
-        //   author: 'John Doe'
-        // },
-        // {
-        //   title: 'Second Blog Post',
-        //   content: 'This is the content of the second blog post.',
-        //   author: 'Jane Smith'
-        // },
-        // {
-        //   title: 'Third Blog Post',
-        //   content: 'This is the content of the third blog post.',
-        //   author: 'Bob Johnson'
-        // },
-        // {
-        //   title: 'First Blog Post',
-        //   content: 'This is the content of the first blog post.',
-        //   author: 'John Doe'
-        // },
-        // {
-        //   title: 'Second Blog Post',
-        //   content: 'This is the content of the second blog post.',
-        //   author: 'Jane Smith'
-        // },
-        // {
-        //   title: 'Third Blog Post',
-        //   content: 'This is the content of the third blog post.',
-        //   author: 'Bob Johnson'
-        // },
-        // Add 12 more blog post objects here
+        }
       ];
     // then we delete every fruit in the database(all instances of this resource)
     // this will delete any blogs that are not owned by a user
@@ -142,4 +158,4 @@ db.on('open', () => {
             // always make sure to close the connection
             db.close()
         })
-})
+});
